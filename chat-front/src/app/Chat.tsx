31fdd3bb5b74ffb942608fba69fa2016a20311d9
hated <yobenali@student.1337.ma>
2024-07-16 17:44:09 +0100

    the status has been crafted and the details are good except the mail redirection

--- chat-front/src/app/Chat.tsx
+++ chat-front/src/app/Chat.tsx
@@ -3,7 +3,7 @@ const Chat = () => {
         <div className="chat" >
             <h2 style={{color: '#1B1B1D', paddingLeft: '10vw'}}>Start a Conversation</h2>
             <div style={{height: '82vh', backgroundColor: 'white', borderRadius: '25px', padding: '0 4px 0 4px', opacity: '0.9', margin: '1%'}}></div>
-            <input type="text" placeholder="messaging" style={{height: '4vh', borderRadius: '10px', paddingLeft: '5%', width: '38dvw', opacity: '0.5', marginTop: '1%'}} />
+            <input type="text" placeholder="messaging" style={{height: '4vh', borderRadius: '10px', paddingLeft: '5%', width: '38dvw', opacity: '0.5', margin: '1%'}} />
             <button style={{color: '#1B1B1D', height: '4vh', width: '8vw', borderRadius: '10px'}}>Send</button>
         </div>
     )
